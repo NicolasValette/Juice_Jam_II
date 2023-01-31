09086c6609088a98ca6df6673bc35d28d7674376
RicardoLuqueMartos <lmricmail@yahoo.fr>
2023-01-31 12:00:37 +0100

    Game Settings Inspector button created (to Detect all content)

--- Assets/Scripts/Ricardo Scripts/Datas/GameData.cs
+++ Assets/Scripts/Ricardo Scripts/Datas/GameData.cs
@@ -2,6 +2,7 @@ using OpenCover.Framework.Model;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class GameData : ScriptableObject
@@ -20,20 +21,23 @@ public class GameData : ScriptableObject
     public class ContentData
     {
         [SerializeField]
-        List<LevelData> levelsList;
-        public List<LevelData> _levelsList { get { return levelsList; } }
+        public List<LevelData> levelsList;
+     //   public List<LevelData> _levelsList { get { return levelsList; } }
 
         [SerializeField]
-        List<WeaponData> weaponsList;
-        public List<WeaponData> _weaponsList { get { return weaponsList; } }
+        public List<WeaponData> weaponsList;
+   //     public List<WeaponData> _weaponsList { get { return weaponsList; } }
 
         [SerializeField]
-        List<PetData> petsList;
-        public List<PetData> _petsList { get { return petsList; } }
+        public List<PetData> petsList;
+    //    public List<PetData> _petsList { get { return petsList; } }
 
         [SerializeField]
-        List<EnemyData> enemiesList;
-        public List<EnemyData> _enemiesList { get { return enemiesList; } }
+        public List<EnemyData> enemiesList;
+        //   public List<EnemyData> _enemiesList { get { return enemiesList; } }
+        [SerializeField]
+        public List<MusicData> musicsList;
+        //   public List<EnemyData> _enemiesList { get { return enemiesList; } }
 
     }
     [SerializeField]
@@ -42,5 +46,111 @@ public class GameData : ScriptableObject
 
     #endregion Variables
 
+#if UNITY_EDITOR
 
+    [MenuItem("Window/MyEditor/Detect All Datas")]
+    public void DetectAllDatas()
+    {
+        InitLists();
+
+        DetectAllPetsDatas();
+        DetectAllWeaponsDatas();
+        DetectAllEnemysDatas();
+        DetectAllLevelsDatas();
+        DetectAllMusicsDatas();
+
+        Save();
+    }
+
+    void InitLists()
+    {
+        if (gameContent.petsList == null)
+            gameContent.petsList = new List<PetData>();
+        if (gameContent.weaponsList == null)
+            gameContent.weaponsList = new List<WeaponData>();
+        if (gameContent.enemiesList == null)
+            gameContent.enemiesList = new List<EnemyData>();
+        if (gameContent.levelsList == null)
+            gameContent.levelsList = new List<LevelData>();
+        if (gameContent.musicsList == null)
+            gameContent.musicsList = new List<MusicData>();
+    }
+
+    public void DetectAllPetsDatas()
+    {
+        string[] guids = AssetDatabase.FindAssets("t:PetData", null);
+        foreach (string guid in guids)
+        {
+            PetData asset = (PetData)AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(guid), typeof(PetData));
+
+            if (gameContent.petsList.Contains(asset) == false)
+            {
+                gameContent.petsList.Add(asset);
+            }
+        }
+    }
+
+    public void DetectAllWeaponsDatas()
+    {
+        string[] guids = AssetDatabase.FindAssets("t:WeaponData", null);
+        foreach (string guid in guids)
+        {
+            WeaponData asset = (WeaponData)AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(guid), typeof(WeaponData));
+
+            if (gameContent.weaponsList.Contains(asset) == false)
+            {
+                gameContent.weaponsList.Add(asset);
+            }
+        }
+    }
+
+    public void DetectAllEnemysDatas()
+    {
+        string[] guids = AssetDatabase.FindAssets("t:EnemyData", null);
+        foreach (string guid in guids)
+        {
+            EnemyData asset = (EnemyData)AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(guid), typeof(EnemyData));
+
+            if (gameContent.enemiesList.Contains(asset) == false)
+            {
+                gameContent.enemiesList.Add(asset);
+            }
+        }
+    }
+
+    public void DetectAllLevelsDatas()
+    {
+        string[] guids = AssetDatabase.FindAssets("t:LevelData", null);
+        foreach (string guid in guids)
+        {
+            LevelData asset = (LevelData)AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(guid), typeof(LevelData));
+
+            if (gameContent.levelsList.Contains(asset) == false)
+            {
+                gameContent.levelsList.Add(asset);
+            }
+        }
+    }
+
+    public void DetectAllMusicsDatas()
+    {
+        string[] guids = AssetDatabase.FindAssets("t:MusicData", null);
+        foreach (string guid in guids)
+        {
+            MusicData asset = (MusicData)AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(guid), typeof(MusicData));
+
+            if (gameContent.musicsList.Contains(asset) == false)
+            {
+                gameContent.musicsList.Add(asset);
+            }
+        }
+    }
+
+
+    public void Save()
+    {
+        EditorUtility.SetDirty(this);
+        AssetDatabase.SaveAssets();
+    }
+#endif
 }
